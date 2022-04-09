8ce0015babb8ba4912350599df8081522aa22193
Moises Vidal <moises.vidal@celfocus.com>
2022-04-09 12:15:50 +0100

    Initial commit

---
+++ src/test/java/com/company/stockchecker/StockCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.stockchecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
