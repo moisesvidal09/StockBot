018eb3c57c61f435fb3a4798cfd682914fea53ec
Moises Vidal <moises.vidal@celfocus.com>
2022-04-24 19:21:17 +0100

    Added stock service test

--- src/test/java/com/company/stockchecker/service/StockServiceTest.java
+++ src/test/java/com/company/stockchecker/service/StockServiceTest.java
@@ -1,31 +1,88 @@
 package com.company.stockchecker.service;
-
-import com.company.stockchecker.entity.dto.StockDTO;
+import com.company.stockchecker.config.StockConfig;
+import com.company.stockchecker.entity.Stock;
+import com.company.stockchecker.repository.StockRepository;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
-import java.util.Collections;
-import java.util.List;
 
-@SpringBootTest
-@RunWith(SpringRunner.class)
+import javax.persistence.EntityNotFoundException;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
 @ActiveProfiles("test")
 public class StockServiceTest {
 
-   @Autowired
    private StockService stockService;
 
-   @Test
-   public void shouldRetrieveBBAS3SAStock(){
+   @Mock
+   private RestService restService;
 
-      List<StockDTO> stocks = stockService.getStocksFromAPI(Collections.singletonList("BBAS3.SA"));
+   @Mock
+   private StockConfig stockConfig;
 
-       Assert.assertEquals("BBAS3.SA", stocks.stream().findAny().get().getSymbol());
+   @Mock
+   private StockRepository stockRepository;
+
+
+   @Before
+   public void setup(){
+      this.stockService = new StockService(this.restService, this.stockConfig, this.stockRepository);
    }
 
+   @Test
+   public void shouldSaveStock() {
+      when(stockRepository.save(any(Stock.class)))
+              .thenReturn(Stock.builder()
+                                 .id(randomLong())
+                                 .code("BBAS3")
+                                 .build());
+
+      final Stock stock = Stock.builder()
+              .code("BBAS3")
+              .build();
+
+      final Long id = stockService.create(stock);
+
+      verify(stockRepository, times(1)).save(any());
+
+      ArgumentCaptor<Stock> captor = ArgumentCaptor.forClass(Stock.class);
+      verify(stockRepository).save(captor.capture());
+      Assert.assertTrue(captor.getValue().getCode().equalsIgnoreCase(stock.getCode()));
+
+      org.assertj.core.api.BDDAssertions.then(id)
+                      .as("Checking that Stock ID is set when stored.")
+                              .isNotNull();
+   }
+
+   @Test
+   public void shouldThrownEntityNotFoundWhenStockNotExist(){
+
+      Long stockId = new Random().nextLong();
+
+      assertThatThrownBy(() -> this.stockService.getById(stockId))
+              .isInstanceOf(EntityNotFoundException.class)
+              .hasMessageContaining("Was not possible to find stock with id = "+stockId);
+   }
+
+   private long randomLong() {
+      return ThreadLocalRandom.current().nextLong(1000L);
+   }
 
 }
